3047810f24e9295a47e87f358f7c4d28a9a05eee
Rehket <aalbright425@gmail.com>
2020-07-30 21:03:35 -0400

    Target spec set!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 // First is deal with the Standard Library...
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main]  // disable all Rust-level entry points
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     loop{}
 }
