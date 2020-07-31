99daa23afa9c8210aa188ac566b347f843b14bd5
Rehket <aalbright425@gmail.com>
2020-07-30 21:15:04 -0400

    Tweaking build settings, mostly local

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // First is deal with the Standard Library...
 #![no_std] // don't link the Rust standard library
 #![no_main]  // disable all Rust-level entry points
+extern crate rlibc;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
