0bdb59a855b6a4778826012128bab70b168d0974
Rehket <aalbright425@gmail.com>
2020-07-30 20:44:50 -0400

    Init Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// First is deal with the Standard Library...
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
