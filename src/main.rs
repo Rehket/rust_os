c74e485dbaf7182a2c7ba0bca85321db4e5f1104
Rehket <aalbright425@gmail.com>
2020-07-30 21:18:50 -0400

    Printing to Screen example

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
+    
     loop{}
 }
